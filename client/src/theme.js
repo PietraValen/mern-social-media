ad7eba0dd4a6076b1c106694706c4d9574ddeae7
Pietra Valentina <100318524+PietraValen@users.noreply.github.com>
2024-03-08 07:48:46 -0300

    Atualizar o theme.js

--- client/src/theme.js
+++ client/src/theme.js
@@ -38,7 +38,7 @@ export const themeSettings = (mode) => {
         ? {
             // palette values for dark mode
             primary: {
-              dark: colorTokens.primary[200],
+              dark: colorTokens.primary[300],
               main: colorTokens.primary[500],
               light: colorTokens.primary[800],
             },
